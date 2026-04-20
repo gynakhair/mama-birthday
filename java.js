b5555604918e199d1938524db9e184485ab1336c
gynakhair <gynaganaga@gmail.com>
2026-04-20 12:06:18 +0700

    fix subtitle + mobile view

--- java.js
+++ java.js
@@ -1,6 +1,28 @@
-onload = () => {
-  const c = setTimeout(() => {
+window.addEventListener("load", () => {
+  setTimeout(() => {
     document.body.classList.remove("not-loaded");
-    clearTimeout(c);
-  }, 1000);
-};
+  }, 500);
+});
+
+const text = `Terima kasih selalu memberi kasih sayang dan perhatian.
+Hidupku akan selalu baik-baik saja jika di dunia ini masih ada Mamah.
+Maka dari itu, semoga mamah selalu sehat, panjang umur, dilancarkan rezekinya dan diberkahi hidupnya, 
+we love you, Mah! 
+-Junaedi Family`;
+
+let i = 0;
+const speed = 40;
+
+function typeWriter() {
+  const el = document.querySelector(".subtitle");
+
+  if (i === 0) el.style.opacity = "1";
+
+  if (i < text.length) {
+    el.innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+setTimeout(typeWriter, 2000);
